de77102b8c8c2d39f135e01618d69c320296718c
unknown <sallukhan54159@gmail.com>
2024-09-06 23:26:59 +0530

    hii

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,17 @@
+import { HStack,Img } from "@chakra-ui/react"
+
+  import logo from "../assets/logo.webp"
+
+const NavBar = () => {
+  return (
+      <HStack>
+  
+      <Img src={logo} />
+
+
+      </HStack>
+  
+  )
+}
+
+export default NavBar
